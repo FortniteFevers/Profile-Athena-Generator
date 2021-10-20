66d99ef4652f7ad13ff138fa0d9eb74f2b32a11e
Fevers <47673608+FortniteFevers@users.noreply.github.com>
2021-10-20 16:42:16 -0400

    added variants. your welcome.

--- gen.py
+++ gen.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import time
 
 print('Profile Athena Generator - by fevers')
 
@@ -26,10 +27,15 @@ a_file.close()
 
 if ioption == '1':
     response = requests.get('https://benbot.app/api/v1/cosmetics/br?lang=en')
-else:
+elif ioption == '2':
     response = requests.get('https://benbot.app/api/v1/newCosmetics?lang=en')
+elif ioption == '3':
+    print('What set do u wanna grab?')
+    ask = input('>> ')
+    response = requests.get(f'https://fortnite-api.com/v2/cosmetics/br/search/all?set={ask}')
 
 if ioption == '1': # All items
+    start = time.time()
     for i in response.json():
         id = i['id']
         backendtype = i['backendType']
@@ -66,15 +72,60 @@ if ioption == '1': # All items
             }]
             for i in i['variants'][0]['options']:
                 tag = i['tag']
-                json_object['items'][id]['attributes']['variants'][0]['owned'] = [
-                    f"{tag}"
-                ]
+                json_object['items'][id]['attributes']['variants'][0]['owned'].append(
+                    f'{tag}'
+                )
+
+    end = time.time()
+    print(f'Generated in {round(end - start, 2)} seconds')
             
 
-else: # New items
+elif ioption == '2': # New items
     for i in response.json()['items']:
         id = i['id']
         backendtype = i['backendType']
+        id = f'{backendtype}:{id}'
+
+        json_object['items'][id] = {
+            'attributes': {
+                "favorite": False,
+                "item_seen": True,
+                "level": 1,
+                "max_level_bonus": 0,
+                "rnd_sel_cnt": 0,
+                "variants": [],
+                "xp": 0
+            },
+            'templateId': f'{id}'
+        }
+        try:
+            variants = True
+            channel = i['variants'][0]['channel']
+            active = i['variants'][0]['options'][0]['tag']
+        except:
+            variants = None
+
+        if variants == True:
+            print(i['id'])
+            channel = i['variants'][0]['channel']
+            active = i['variants'][0]['options'][0]['tag']
+
+            json_object['items'][id]['attributes']['variants'] = [{
+               "active": f"{active}",
+               "channel": f"{channel}",
+               "owned": []
+            }]
+            for i in i['variants'][0]['options']:
+                tag = i['tag']
+                json_object['items'][id]['attributes']['variants'][0]['owned'].append(
+                    f'{tag}'
+                )
+
+
+elif ioption == '3': # Set items
+    for i in response.json()['data']:
+        id = i['id']
+        backendtype = i['type']['backendValue']
         id = f'{backendtype}:{id}'
 
         json_object['items'][id] = {
@@ -116,3 +167,4 @@ a_file = open(f"profile_athena.json", "w")
 json.dump(json_object, a_file, indent = 4)
 
 print('\nGenerated!')
+time.sleep(5)
